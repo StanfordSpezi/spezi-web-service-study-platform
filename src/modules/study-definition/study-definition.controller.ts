c3e1fec5d760d719c8b7fd9a0225a14154e447c0
Warren Hiemstra <wahiemstra@lh.ca>
2025-06-11 11:23:42 -0400

    Added getById to controller and improved Swagger docs

--- src/modules/study-definition/study-definition.controller.ts
+++ src/modules/study-definition/study-definition.controller.ts
@@ -4,9 +4,23 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from "@nestjs/common";
 import { StudyDefinitionService } from "./study-definition.service";
 import { CreateStudyDefinitionDto } from "./dto/create-study-definition.dto";
+import {
+  ApiParam,
+  ApiOkResponse,
+  ApiBody,
+  ApiCreatedResponse,
+} from "@nestjs/swagger";
+import { GetStudyDefinitionDto } from "./dto/get-study-definition.dto";
 
 @Controller("study-definition")
 export class StudyDefinitionController {
@@ -14,8 +28,35 @@ export class StudyDefinitionController {
     private readonly studyDefinitionService: StudyDefinitionService,
   ) {}
 
+  @Get(":id")
+  @ApiParam({ name: "id", description: "StudyDefinition UUID" })
+  @ApiOkResponse({
+    description:
+      "Returns a full study definition with components and metadata.",
+    type: GetStudyDefinitionDto,
+  })
+  async getById(
+    @Param("id", new ParseUUIDPipe({ version: "4" })) id: string,
+  ): Promise<GetStudyDefinitionDto> {
+    return this.studyDefinitionService.getById(id);
+  }
+
   @Post()
-  async create(@Body() dto: CreateStudyDefinitionDto) {
-    return this.studyDefinitionService.create(dto);
+  @ApiBody({
+    description:
+      "Create a new study definition with components, criteria, and optional enrollment conditions.",
+    type: CreateStudyDefinitionDto,
+  })
+  @ApiCreatedResponse({
+    description: "The study was successfully created.",
+    schema: {
+      example: {
+        id: "dc58628c-b92f-4c3e-b8d1-798f01fa8e2e",
+      },
+    },
+  })
+  async create(@Body() dto: CreateStudyDefinitionDto): Promise<{ id: string }> {
+    const id = await this.studyDefinitionService.create(dto);
+    return { id };
   }
 }
